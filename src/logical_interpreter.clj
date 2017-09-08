a31b087dbd82041bf4e6cdf29692efc94efae243
guidonegri <bokita_93@hotmail.com>
2017-09-07 23:12:08 -0300

    logical-interpreter pass all test

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,126 @@
 (ns logical-interpreter)
 
+(require '[clojure.string :as str])
+
+;PATTERNS:
+(def query-pattern (re-pattern "(.*)\\((.*)\\)"))
+(def fact-pattern (re-pattern "(.*)\\((.*)\\)"))
+(def rule-pattern (re-pattern "(.*)\\((.*)\\) :- (.*)"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;GETTERS:
+(defn get-name-of
+  ;Returns the name of a query, fact or rule. e.g: "varon(jorge)" returns "varon" 
+  [item pattern]
+  (second (re-find pattern item))
+)
+
+(defn get-params-of
+  ;Returns vector with the params of a query or rule. e.g: "hijo(X, Y) :- varon(X), padre(Y, X)" returns "["X" "Y"]"
+  [item pattern]
+  (str/split (nth (re-find pattern item) 2) #", " )
+)
+
+(defn get-facts-of-rule
+  ;Returns the facts of a rule. e.g: "hijo(X, Y) :- varon(X), padre(Y, X)" returns "varon(X), padre(Y, X)"
+  [rule]
+  (nth (re-find rule-pattern rule) 3)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;VALIDATES:
+(defn is-valid-database
+  [db]
+  (distinct? (count (filter #(re-matches fact-pattern %) db)) (count db) )
+)
+
+(defn is-invalid-query
+  [query]
+  (nil? (re-matches fact-pattern query))
+)
+
+(defn is-rule-in-database
+  [query db]
+  (let [query-name (get-name-of query query-pattern)]
+    (> (count (filter (fn[item](= query-name (get-name-of item rule-pattern))) db)) 0)
+  )
+)
+
+(defn is-fact-in-database
+  [query db]
+  (let [query-name (get-name-of query query-pattern)]
+    (> (count (filter (fn[item](= query-name (get-name-of item fact-pattern))) db)) 0 )
+  )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;FOR FACTS:
+(defn query-fact
+  ;If query matches with fact in DB, returns True. If not, False.
+  [db query]
+  (= (count (filter (fn [fact] (= query fact)) db) ) 1)
+)
+
+;FOR RULES:
+(defn replace-rule-with-query-params
+  ;Recursive function. Returns the complete rule with the query parameters.
+  [rule index params-db params-query]
+  (let [param (re-pattern (nth params-db index))
+        rule-with-query-params (str/replace rule param (nth params-query index))]
+    (cond
+	    (= index (- (count params-db) 1) ) rule-with-query-params
+	    :else (replace-rule-with-query-params rule-with-query-params (+ index 1) params-db params-query)
+    )
+  ) 
+)
+
+(defn evaluate-facts-of-rule
+  ;Evaluates each fact of rule facts. Returns a list with the facts evaluate.
+  [db rule-facts]
+  (for [fact rule-facts
+      :let [result (query-fact db fact)]
+      ]
+  result)
+)
+
+(defn query-rule
+  ;If all facts of rule are true, returns True. If not, False.
+  [db query]
+  (let [name-query (get-name-of query query-pattern)
+        rule (first (filter (fn[item](= name-query (get-name-of item rule-pattern))) db )) ;Complete rule
+        name-rule (get-name-of rule rule-pattern)
+        
+        params-query-rule (get-params-of query query-pattern)
+        params-db-rule (get-params-of rule rule-pattern)
+        
+        rule-with-query-params(replace-rule-with-query-params rule 0 params-db-rule params-query-rule)
+        rule-facts (get-facts-of-rule rule-with-query-params)
+        
+        rule-facts-r (str/replace rule-facts #"\)," ")-") ;Change facts rule separator: , for -
+        rule-facts-s (str/split rule-facts-r #"- ")] ;Split with the new separator
+    
+    (reduce (fn [a b] (and a b)) (evaluate-facts-of-rule db rule-facts-s) )    
+  ) 
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;MAIN:
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
-  [database query]
-  nil)
+  [db-file query]
+  (let [db-str (slurp (java.io.FileReader. db-file))
+        db (str/split db-str #".\n")]                
+    (cond
+      (is-valid-database db) nil
+      (is-invalid-query query) nil
+      (is-fact-in-database query db) (query-fact db query)
+      (is-rule-in-database query db) (query-rule db query)
+      :else false
+    ) 
+  )  
+)
